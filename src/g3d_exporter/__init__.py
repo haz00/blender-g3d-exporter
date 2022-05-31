b6b57f415b3a2ac8165071d410d819f29c0de6c6
haz00 <haz00ku@gmail.com>
2022-05-31 11:31:58 +0400

    bump version

--- src/g3d_exporter/__init__.py
+++ src/g3d_exporter/__init__.py
@@ -4,7 +4,7 @@ bl_info = {
     "name": "LibGDX G3D Exporter",
     "description": "Export scene to LibGDX compatible format",
     "author": "haz00",
-    "version": (0, 3),
+    "version": (0, 4),
     "blender": (2, 93, 0),
     "location": "File > Export",
     "category": "Import-Export",
